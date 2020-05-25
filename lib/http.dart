0cf0ec7856ac0b303e9cab020e24a8c69f5a3d97
f3ath <karapetov@gmail.com>
2020-05-25 12:55:17 -0700

    Export http.Method

--- lib/http.dart
+++ lib/http.dart
@@ -5,4 +5,5 @@ export 'package:json_api_common/src/http/http_handler.dart';
 export 'package:json_api_common/src/http/http_request.dart';
 export 'package:json_api_common/src/http/http_response.dart';
 export 'package:json_api_common/src/http/logging_http_handler.dart';
+export 'package:json_api_common/src/http/method.dart';
 export 'package:json_api_common/src/http/transforming_http_handler.dart';
